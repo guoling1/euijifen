067dbf4800a248d844aa58f05395c570fcf31334
guoling1 <646626114@qq.com>
2018-08-11 10:06:33 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,78 @@
+﻿/*rem*/
+(function () {
+    var t;
+
+    function initHtmlFont() {
+        var maxWidth = 750;
+        var html = document.documentElement;
+        var windowWidth = html.clientWidth;
+        windowWidth = html.clientWidth > maxWidth ? maxWidth : html.clientWidth;
+        html.style.fontSize = (windowWidth / 375) * 100 + 'px';
+    }
+    window.onresize = function () {
+        clearTimeout(t);
+        t = setTimeout(initHtmlFont, 250);
+    }
+    initHtmlFont();
+})();
+
+var ip = 'http://192.168.9.9';
+
+function getURl(links, data, type, cb) {
+    var id = checkStorage();
+    var domImg = '';
+    domImg += '<div class="loadimg">';
+    domImg += '<img src="img/loading.gif" />';
+    domImg += '</div>';
+    $('body').append(domImg);
+    $.ajax({
+        type: type,
+        url: ip + links,
+        async: true,
+        data: data,
+        headers: {
+            userid: id,
+            type: 'web',
+            platform: 6
+        },
+        success: function (s) {
+            if (typeof (s) == 'object') {
+                cb(s);
+            } else {
+                s = JSON.parse(s);
+                cb(s);
+            };
+        },
+        complete: function () {
+            setTimeout(function () {
+                $('.loadimg').remove();
+            }, 500);
+        },
+        error: function (xhr, status) {
+            if (xhr.status == 0) {
+                setTimeout(function () {
+                    location.reload();
+                }, 300);
+            }
+        }
+    });
+}
+
+function getData(links, data, cb) {
+    getURl(links, data, 'get', function (s) {
+        cb(s);
+    });
+};
+
+function postData(links, data, cb) {
+    getURl(links, data, 'post', function (s) {
+        cb(s);
+    });
+};
+//截取url数据方法
+var getParam = function (name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+};
---
+++ js/public.js
@@ -0,0 +1,68 @@
+// 请求
+var ip = "http://www.eduijifen.com";
+function getData(link, type, data, cb) {
+    $.ajax({
+        type: type,
+        url: ip + link,
+        data: data,
+        success: function (s) {
+            s=JSON.parse(s);
+            cb(s)
+        }
+    })
+}
+//截取url数据方法
+var getParam = function GetRequest() {
+    var url = location.search; //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
+};
+//判断用户是否登录
+function judgingLogon() {
+    var user = localStorage.getItem("user");
+    if(user==null){
+        location.href="login.html"
+    }
+};
+var userMessage = JSON.parse(localStorage.getItem("user"));
+
+//发送验证码
+var countdown = 60;
+function sendemail(){
+    var obj = $("button");
+    settime(obj);
+
+}
+function settime(obj) { //发送验证码倒计时
+    if (countdown == 0) {
+        obj.attr('disabled',false);
+        obj.html("重新获取");
+        countdown = 60;
+        return;
+    } else {
+        obj.attr('disabled',true);
+        obj.html(countdown+"s");
+        countdown--;
+    }
+    setTimeout(function() {
+            settime(obj) }
+        ,1000)
+}
+function sendCode(phone,type) {
+    getData("/ajax/sms.ashx","post",{action:"getsmscode",accountid:phone,smstype:type},function (res) {
+        if(res.success){
+            sendemail()
+            // alert("验证码发送成功");
+        }else {
+            alert(res.msg)
+        }
+    })
+}
+var phoneReg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
